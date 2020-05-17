08fa2558c5715ba34690c2f416144ca50f2f1007
DianDelph <da.dnvl@live.com>
2020-05-17 13:45:54 -0500

    Agrega lógica de ahorcado. Termina juego

--- ahorcado.py
+++ ahorcado.py
@@ -96,12 +96,41 @@ def random_word():
 
 def run():
 	word = random_word ()
-	hidden_word = ['_'] * len(word) # genera un tablero, su largo es el mismo que la palabra escondida
+	# genera un tablero, su largo es el mismo que la palabra escondida
+	hidden_word = ['_'] * len(word)
+	# genera un tablero, su largo es el mismo que la palabra escondida
+	hidden_word = ['_'] * len(word)
 	tries = 0 #guarda el número de errores
 
 	while True:  #loop infinito
 		display_board(hidden_word, tries) # va a manejar la interfaz
-		current_letter = str(input('Escoge una letra: '))
+		current_letter = input('Escoge una letra: ')
+
+		letter_indexes = [] 
+		for index in range(len(word)): # veremos si la letra ingresada corresponde
+			if word[index] == current_letter:
+				letter_indexes.append(index) # si la letra corresponde, la inserta donde le toca
+
+		if len(letter_indexes) == 0 :# si la lista está vacía, el usuario no le atinó
+			tries += 1
+
+			if tries == 7:
+				display_board(hidden_word, tries)
+				print('')
+				print(' ¡Perdiste! La palabra correcta era {}'.format(word))
+				break
+		else:
+			for index in letter_indexes:
+				hidden_word[index] = current_letter #se muestran las letras que ya llevo
+				
+			letter_indexes = [] # comienzo de nuevo
+		try:
+			hidden_word.index('_')  #busca si quedan letras por adivinar
+		except ValueError:
+			print(' ')
+			print('¡Felicidades! Ganaste. La palabra es {}'.format(word))
+			break
+				
 
 if __name__ == "__main__":
 	print (' B I E N V E N I D O S   A  A H O R C A D O')
